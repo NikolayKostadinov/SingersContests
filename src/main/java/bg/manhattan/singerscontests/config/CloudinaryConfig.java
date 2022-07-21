d0890f0d7157cd508785a6cca9c459609164a2eb
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-07-21 16:43:48 +0300

    Start of File upload functionality implementation

---
+++ src/main/java/bg/manhattan/singerscontests/config/CloudinaryConfig.java
@@ -0,0 +1,25 @@
+package bg.manhattan.singerscontests.config;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class CloudinaryConfig {
+    private final Environment env;
+    public CloudinaryConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public Cloudinary getCloudinary() {
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", env.getProperty("cloudinary.cloud_name"),
+                "api_key", env.getProperty("cloudinary.api_key"),
+                "api_secret", env.getProperty("cloudinary.api_secret"),
+                "secure", env.getProperty("cloudinary.secure")));
+    }
+}
